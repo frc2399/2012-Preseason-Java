742c872f6aa2eb58d8c9c14c7cc86e8292bc9e99
Ed Venator <esv@case.edu>
2011-11-05 11:45:03 -0400

    Did drive two ways as an example.

--- src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
+++ src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
@@ -9,6 +9,10 @@ package edu.wpi.first.wpilibj.templates;
 
 
 import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.DriverStationLCD;
+import edu.wpi.first.wpilibj.Jaguar;
+import edu.wpi.first.wpilibj.RobotDrive;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -18,12 +22,37 @@ import edu.wpi.first.wpilibj.IterativeRobot;
  * directory.
  */
 public class RobotTemplate extends IterativeRobot {
+    //Drivers station LCD for debug statements while driving
+    DriverStationLCD lcd;
+    
+    //Robotdrive object for controlling drivetrain
+    RobotDrive driveTrain;
+    //Drivetrain motor controllers
+    Jaguar frontRightJag, frontLeftJag, backRightJag, backLeftJag;
+    
+    //Joysticks for tank drive
+    Joystick leftStick, rightStick;
+    
+    //Joystick for mecanum drive
+    Joystick twistStick;
+    
     /**
      * This function is run when the robot is first started up and should be
      * used for any initialization code.
      */
     public void robotInit() {
-
+        //Instantiate all objects
+        leftStick = new Joystick(1);
+        rightStick = new Joystick(2);
+        twistStick = new Joystick(3);
+        
+        frontRightJag = new Jaguar(1);
+        frontLeftJag = new Jaguar(2);
+        backRightJag = new Jaguar(3);
+        backLeftJag = new Jaguar(4);
+        
+        driveTrain = new RobotDrive(frontLeftJag, backLeftJag, frontRightJag, backRightJag);
+        
     }
 
     /**
@@ -37,7 +66,21 @@ public class RobotTemplate extends IterativeRobot {
      * This function is called periodically during operator control
      */
     public void teleopPeriodic() {
-        printf("test");
+        //This line is used to drive the robot in a tank drive configuration
+        driveTrain.tankDrive(leftStick, rightStick);
+        
+        //This line is used to drive the robot in all directions
+        mecanumDrive(twistStick);
     }
     
+    /**
+     * This function is used to drive the robot in all directions with 
+     * mecanum wheels
+     */
+    private void mecanumDrive(Joystick stick){
+        double dtRotation = stick.getZ();
+        double dtX = stick.getX();
+        double dtY = stick.getY();
+        driveTrain.mecanumDrive_Cartesian(dtX, dtY,dtRotation, 0);
+    }
 }
