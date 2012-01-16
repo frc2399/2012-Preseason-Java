7db496910abaa6dbf1798980601f678e8bb7bf09
unknown <Gillie@Gillie-VAIO.(none)>
2012-01-16 10:41:27 -0500

    preseason code with Genghis tank drive tacked on

--- src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
+++ src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
@@ -7,14 +7,9 @@
 
     package edu.wpi.first.wpilibj.templates;
 
-
     import edu.wpi.first.wpilibj.IterativeRobot;
     import edu.wpi.first.wpilibj.Joystick;
     import edu.wpi.first.wpilibj.RobotDrive;
-    import edu.wpi.first.wpilibj.Jaguar; 
-    import edu.wpi.first.wpilibj.DigitalInput;
-    import edu.wpi.first.wpilibj.AnalogChannel;
-
     /**
      * The VM is configured to automatically run this class, and to call the
      * functions corresponding to each mode, as described in the IterativeRobot
@@ -24,6 +19,57 @@
      */
     public class RobotTemplate extends IterativeRobot {
 
+        Joystick leftStick, rightStick;
+        RobotDrive dt; 
+
+
+        /**
+         * This function is run when the robot is first started up and should be
+         * used for any initialization code.
+         */
+        public void robotInit() {
+            leftStick = new Joystick(1);
+            rightStick = new Joystick(2);
+            dt = new RobotDrive(5,4,1,2);
+          
+        }
+
+        /**
+         * This function is called periodically during autonomous
+         */
+        public void autonomousPeriodic() {
+
+        }
+
+        /**
+         * This function is called periodically during operator control
+         */
+        public void teleopPeriodic() {
+            double yLeft, yRight;
+            yLeft = -leftStick.getRawAxis(2);
+            yRight = -rightStick.getRawAxis(2);
+            dt.tankDrive(yLeft,yRight); 
+        }
+            
+    }
+//Normal Code begins here!
+   /*
+    import edu.wpi.first.wpilibj.IterativeRobot;
+    import edu.wpi.first.wpilibj.Joystick;
+    import edu.wpi.first.wpilibj.RobotDrive;
+    import edu.wpi.first.wpilibj.Jaguar; 
+    import edu.wpi.first.wpilibj.DigitalInput;
+    import edu.wpi.first.wpilibj.AnalogChannel;
+   
+    /**
+     * The VM is configured to automatically run this class, and to call the
+     * functions corresponding to each mode, as described in the IterativeRobot
+     * documentation. If you change the name of this class or the package after
+     * creating this project, you must also update the manifest file in the resource
+     * directory.
+     *
+    public class RobotTemplate extends IterativeRobot {
+  
         Joystick armStick, driveStick;
         RobotDrive dt;
         Jaguar armJag; 
@@ -36,7 +82,7 @@
         /**
          * This function is run when the robot is first started up and should be
          * used for any initialization code.
-         */
+         *
         public void robotInit() {
             armStick = new Joystick(1);
             driveStick = new Joystick(2);
@@ -51,14 +97,14 @@
 
         /**
          * This function is called periodically during autonomous
-         */
+         *
         public void autonomousPeriodic() {
 
         }
 
         /**
          * This function is called periodically during operator control
-         */
+         *
         public void teleopPeriodic() {
             double x, y, rotation;
             x = driveStick.getRawAxis(1); 
@@ -69,6 +115,8 @@
             if(armLimit.get()|| (armSpeed >= 0)){
                 armJag.set(armSpeed);
             }
+             
+             
             if(armStick.getRawButton(3)||grab){
                 clawJag.set(-0.5);//speeds negated; button3 = up button 2 = down
             }else if(armStick.getRawButton(2)||release){
@@ -97,3 +145,5 @@
         }
             
     }
+     * */
+   
